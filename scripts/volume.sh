8a3c3b81d20f1264ead4a074b75c4cde59918178
Jeremy Evans <jeremy@chiefgeek.io>
2024-03-30 21:21:19 -0400

    adding volume script for arch install

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -9,14 +9,11 @@ step=5   # number of percentage points to increase/decrease volume
 
 # Select EITHER the `amixer` or the `pactl` command in each case
 if [ $1 == "raise" ]; then
-  # pactl set-sink-volume @DEFAULT_SINK@ "+${step}%"
-  # amixer set Master ${step}%+ > /dev/null
+  amixer set Master ${step}%+ > /dev/null
 elif [ $1 == "lower" ]; then
-  # pactl set-sink-volume @DEFAULT_SINK@ "-${step}%"
-  # amixer set Master ${step}%- > /dev/null
+  amixer set Master ${step}%- > /dev/null
 elif [ $1 == "mute" ]; then
-  # pactl set-sink-mute @DEFAULT_SINK@ toggle
-  # amixer set Master toggle > /dev/null
+  amixer set Master toggle > /dev/null
 else
   echo "Unrecognized parameter: ${1}"
   echo "Usage should be: volume.sh <raise|lower|mute>"
