29a950ea3ef2ea15204e40abd0cfcc93b4d0c8be
Jeremy Evans <jeremy@chiefgeek.io>
2024-04-01 08:40:09 -0400

    formatter fixed for nvim config

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -75,11 +75,11 @@ return {
 				},
 				-- Use the special "*" filetype for defining formatter configurations on
 				-- any filetype
-				--  ["*"] = {
-				-- "formatter.filetypes.any" defines default configurations for any
-				-- filetype
-				-- require("formatter.filetypes.any").remove_trailing_whitespace
-				-- }
+				["*"] = {
+					-- "formatter.filetypes.any" defines default configurations for any
+					-- filetype
+					require("formatter.filetypes.any").remove_trailing_whitespace,
+				},
 			},
 		})
 	end,
