d333c0468c31b60db424b4e363bea48359e24386
Jeremy Evans <jeremy@chiefgeek.io>
2024-08-06 17:34:20 -0400

    removed -p cider from playerctl, unclear why this was used.

--- waybar/scripts/music_panel.sh
+++ waybar/scripts/music_panel.sh
@@ -4,7 +4,7 @@ CURRENT_SONG="$HOME/.config/waybar/scripts/spotify.sh"
 
 zscroll -p " | " --delay 0.2 \
     --length 40 \
-    --match-command "playerctl -p cider status" \
+    --match-command "playerctl status" \
     --match-text "Playing" "--before-text '<span syle=\"color:#1DB954\">󰓇</span> '" \
     --match-text "Paused" "--before-text ' ' --scroll 0" \
     --update-check true $CURRENT_SONG
