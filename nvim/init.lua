c573dcd5f441abc7b3823fd53dea8c6f4c5e41b6
Jeremy Evans <jeremy@chiefgeek.io>
2024-08-17 13:36:52 -0400

    fixed it again

--- nvim/init.lua
+++ nvim/init.lua
@@ -71,8 +71,8 @@ require("lspconfig").tsserver.setup({
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<Shift-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
     --["<C-y>"] = cmp.mapping.confirm({ select = true }),
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
 	["<C-Space>"] = cmp.mapping.complete(),
