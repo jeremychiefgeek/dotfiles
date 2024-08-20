c9988963a22ee5fe3a1fcb1605e960d0d159e7a1
Jeremy Evans <jeremy@chiefgeek.io>
2024-08-20 13:25:56 -0400

    add tmux support

--- nvim/init.lua
+++ nvim/init.lua
@@ -488,9 +488,12 @@ require("lazy").setup({
 				vim.keymap.set("n", "<leader>a", function()
 					harpoon:list():add()
 				end)
+				--vim.keymap.set("n", "<C-e>", function()
+				--	toggle_telescope(harpoon:list())
+				--end, { desc = "Open harpoon window" })
 				vim.keymap.set("n", "<C-e>", function()
-					toggle_telescope(harpoon:list())
-				end, { desc = "Open harpoon window" })
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end)
 
 				vim.keymap.set("n", "<C-h>", function()
 					harpoon:list():select(1)
@@ -504,6 +507,23 @@ require("lazy").setup({
 				vim.keymap.set("n", "<C-s>", function()
 					harpoon:list():select(4)
 				end)
+			end,
+		},
+		{
+			"alexghergh/nvim-tmux-navigation",
+			config = function()
+				local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+				nvim_tmux_nav.setup({
+					disable_when_zoomed = true, -- defaults to false
+				})
+
+				vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+				vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+				vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+				vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+				vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+				vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
 			end,
 		},
 	},
