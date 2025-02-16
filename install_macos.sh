a523c4fefe27df64ecc10ea61662e7a980b10183
Jeremy Evans <jeremy@chiefgeek.io>
2025-02-16 15:21:11 -0500

    didn't like sf font

--- install_macos.sh
+++ install_macos.sh
@@ -175,10 +175,6 @@ ln -s $HOME/dotfiles/starship.toml $HOME/.config/starship.toml
 ln -s $HOME/dotfiles/borders $HOME/.config/borders
 
 # Installing Fonts
-git clone git@github.com:shaunsingh/SFMono-Nerd-Font-Ligaturized.git /tmp/SFMono_Nerd_Font
-mv /tmp/SFMono_Nerd_Font/* $HOME/Library/Fonts
-rm -rf /tmp/SFMono_Nerd_Font/
-
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.5/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
 source $HOME/.zshrc
