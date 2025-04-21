69ec4022810b2924798bcc3a7aa41ea78e5e84ff
Jeremy Evans <jeremy@chiefgeek.io>
2025-04-21 12:27:12 -0400

    fix for everywhere

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,14 +3,14 @@ return {
     -- "catppuccin/nvim",
     --"shaunsingh/nord.nvim",
     -- "AlexvZyl/nordic.nvim",
-    -- "blazkowolf/gruber-darker.nvim",
-    "rose-pine/neovim",
+    "blazkowolf/gruber-darker.nvim",
+    -- "rose-pine/neovim",
     priority = 1000,
     config = function()
       -- vim.cmd.colorscheme("catppuccin")
       -- vim.cmd.colorscheme("nord")
-      -- vim.cmd.colorscheme("gruber-darker")
-      vim.cmd.colorscheme("rose-pine")
+      vim.cmd.colorscheme("gruber-darker")
+      -- vim.cmd.colorscheme("rose-pine")
     end,
   },
 }
