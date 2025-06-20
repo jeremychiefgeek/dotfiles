d52db3790c71f0c5f8619d5ea0e20bb328937a1c
Jeremy Evans <jeremy@chiefgeek.io>
2025-06-20 09:53:51 -0400

    updated plugins

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "javascript",
+        "typescript",
+        "css",
+        "gitignore",
+        "graphql",
+        "http",
+        "json",
+        "scss",
+        "sql",
+        "vim",
+        "lua",
+        "c_sharp",
+        "c",
+        "vue",
+        "odin",
+        "swift",
+      },
+      auto_install = true,
+      hightlight = {
+        enable = true,
+        additional_vim_regex_hightlighting = { "ruby" },
+      },
+      indent = { enable = true, disable = { "ruby" } },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+    },
+  },
+}
